a159c44c954988897c0dbc0b20374023c150c8ec
kimi8-lapi16 <takayoshi.kimishima@gmail.com>
2024-12-21 19:14:16 +0900

    remove: related token files

--- backend/src/getToken.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from "express";
-
-export default function getTokenFromCookie(req: Request) {
-  console.log('req.cookies.token ---', req.cookies.token);
-  console.log('req.headers["authorization"] ---', req.headers["authorization"]);
-
-  return req.cookies.token;
-}
